fd10f06b5ed92324da599c4826c519e0945d27ed
ken-huang97 <ktkhuang@hotmail.com>
2020-04-14 10:09:09 -0400

    rename dir

--- src/werewolf/werewolf/urls.py
+++ src/werewolf/werewolf/urls.py
@@ -1,7 +1,6 @@
-# mysite/urls.py
 from django.conf.urls import include
-from django.urls import path
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
