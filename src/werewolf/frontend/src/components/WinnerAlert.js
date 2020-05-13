3b3a3a178554f836dbb3ef1bb3a6515f73aa3e84
ktkhuang <ktkhuang@hotmail.com>
2020-05-13 16:55:30 -0400

    ui update missing piece

---
+++ src/werewolf/frontend/src/components/WinnerAlert.js
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function AlertDialog(props) {
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{props.winner + " victory"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Check if your roles changed
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
